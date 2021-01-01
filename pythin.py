d9a30d01d1ad2b0622d483a22bf9b91955179cd2
Harshal Y.y <76250999+Harshal-y@users.noreply.github.com>
2021-01-01 15:17:26 +0530

    Updated pythin.py file and made a simple function to tell the users about *args ,**kwargs

--- pythin.py
+++ pythin.py
@@ -1,1 +1,13 @@
-# Python file
+# A Python file
+if __name__ == '___main__':
+  try:
+    def func1(noranli, *apps, **pasas):
+      print('Remeber that you can take as many args and as many kwargs as you want but normal arguement will not support and changes automatically accordingly u need to do changes manually')
+      print('I am a normal argument', noranli)
+      print('I am an optional argument and can be passed in any way inside this function but when i take this argument i turn the argument in a tuple', type(apps))
+      for i in apps:
+        print(i)
+      for pie, pro in pasas:
+        print(pie, pro)
+  except Exception as exe:
+    print(f'Program crashed because of {str(exe)}')
